718638f006f1e9112cbc5d1d160afdd5c5be3493
ashaansojib <ashaduzzamansojib67@gmail.com>
2025-01-19 11:55:03 +0600

    default website loader

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h2 className='text-red-500'>Your website is loading now! please wait?</h2>
+        </div>
+    );
+};
+
+export default loading;
